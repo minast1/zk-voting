eb39ae67d007f1b326ea49974da769251332db8a
minast1 <edmarfo2@gmail.com>
2026-02-05 00:13:14 +0000

    chore: remove unused functions and events from voting contract

--- packages/foundry/contracts/Voting.sol
+++ packages/foundry/contracts/Voting.sol
@@ -76,7 +76,6 @@ contract Voting is Ownable {
     );
     event VoterAdded(address indexed voter, uint256 indexed poll_id);
     // event NewLeaf(uint256 index, uint256 value);
-    event AllowListRequest(address indexed requester, uint256 timestamp);
 
     event CommitmentRegistered(
         uint256 indexed pollId,
@@ -295,10 +294,6 @@ contract Voting is Ownable {
         voter = s_voters[_voter];
         // /// Checkpoint 2 //////
         registered = s_hasRegistered[pollId][_voter];
-    }
-
-    function requestAllowList() external {
-        emit AllowListRequest(msg.sender, block.timestamp);
     }
 
     function getPollCount() external view returns (uint256) {
